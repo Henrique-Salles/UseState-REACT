9f16c79602e7f0d919daa6d0112a4a862138b21f
Henrique-Salles <henriquesalles1520@gmail.com>
2024-03-12 15:57:46 -0300

    input/setInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+
 export default function App() {
-  return <div></div>;
+  const [input, setInput] = useState("");
+
+  function mostrarAluno() {
+    console.log(input);
+  }
+
+  return (
+    <div>
+      <h1>useState</h1>
+
+      <input
+        placeholder="Digite o nome"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <br />
+
+      <button onClick={mostrarAluno}>Mostrar Aluno</button>
+    </div>
+  );
 }
