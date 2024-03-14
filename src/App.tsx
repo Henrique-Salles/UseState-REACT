6451b25908239ff01b508fddfdaeea0fc38c7881
Henrique-Salles <henriquesalles1520@gmail.com>
2024-03-14 15:05:46 -0300

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ export default function App() {
   const [inputAge, setInputAge] = useState("");
 
   const [info, setInfo] = useState<InfoProps>();
+  const [counter, setCounter] = useState(0);
 
   function mostrarAluno() {
     setInfo({
@@ -18,33 +19,41 @@ export default function App() {
     });
   }
 
+  function add() {
+    setCounter((value) => value + 1);
+  }
+
+  function subtract() {
+    if (counter === 0) return;
+    setCounter((value) => value - 1);
+  }
+
   return (
     <div>
       <h1>useState</h1>
-
       <input
         placeholder="Digite o nome"
         value={inputName}
         onChange={(e) => setInputName(e.target.value)}
       />
-
       <br />
       <br />
-
       <input
         placeholder="Digite a idade"
         value={inputAge}
         onChange={(e) => setInputAge(e.target.value)}
       />
-
       <br />
       <br />
-
       <button onClick={mostrarAluno}>Mostrar Aluno</button>
       <hr />
-
       <h3>Aluno: {info?.name}</h3>
       <h3>Idade: {info?.age}</h3>
+      <hr />
+      <br />
+      <h1>Contador com useState</h1>
+      <button onClick={add}>+</button> {counter}{" "}
+      <button onClick={subtract}>-</button>
     </div>
   );
 }
